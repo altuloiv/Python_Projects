3a6d8c3581bf97f52b5c6995f67ea918420e0873
Albert Tulo <atuloiv@gmail.com>
2022-01-06 15:58:19 -0600

    initial push

---
+++ Class Assignment/PolymorphismAssign.py
@@ -0,0 +1,33 @@
+
+class User:
+    name = "Mark"
+    email = "mark@gmail.com"
+    password = "1234abcd"
+
+    def getLoginInfo(self):
+        entry_name = input("Enter your name: ")
+        entry_email = input("Enter your email: ")
+        entry_password = input("Enter your password: ")
+        if (entry_email == self.email and entry_password == self.password):
+            print("Welcome back, {}!".format(entry_name))
+        else:
+            print("The password or email is incorrect.")
+
+class Employee(User):
+    base_pay = 11.00
+    department = "General"
+    pin_number = "3980"
+
+    def getLoginInfo(self):
+        entry_name = input("Enter your name: ")
+        entry_email = input("Enter your email: ")
+        entry_pin = input("Enter your pin: ")
+        if (entry_email == self.email and entry_pin == self.pin_number):
+            print("Welcome back, {}!".format(entry_name))
+        else: print("The pin or email is incorrect")
+
+customer = User()
+customer.getLoginInfo()
+
+manager = Employee()
+manager.getLoginInfo()
