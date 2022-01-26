32e6fc0f4a12844a9070b881681de88b87b7db49
Albert Tulo <atuloiv@gmail.com>
2022-01-26 17:40:20 -0600

    initial push

--- File GUI Assignment/main.py
+++ File GUI Assignment/main.py
@@ -7,17 +7,18 @@ root = Tk()
 root.title('File Manager')
 root.geometry("500x500")
 
+###choosing the directory for source
 def chooseSource():
     src = filedialog.askdirectory()
     srcEntry.insert(0, src)
 
-
+#####choosing destination directory
 def chooseDest():
     dest = filedialog.askdirectory()
     destEntry.insert(0, dest)
 
 
-
+#####getting the time to read for a day and transfer files if modified every 24 hours
 def transferFiles():
     sourcePath = srcEntry.get()
     destPath = destEntry.get()
@@ -25,7 +26,7 @@ def transferFiles():
     now = time.time()
     before = now - SECONDS_IN_DAY
 
-
+#####checking if files in folder was modified and if was within the time allowed transfers files
     def last_mod_time(fname):
         return os.path.getmtime(fname)
     for fname in os.listdir(sourcePath):
@@ -40,6 +41,7 @@ srcEntry.pack(pady=26)
 destEntry = Entry(root)
 destEntry.pack(pady=28)
 
+#buttons to call commands for file transfer and folder selector
 srcButton = Button(root, text="Source Folder", command=chooseSource)
 srcButton.pack(pady=24)
 destButton = Button(root, text="Destination Folder", command=chooseDest)
